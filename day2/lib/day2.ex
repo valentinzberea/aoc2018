0c15b94e3040dc99c1aac6fbef52fcd6c7995067
Valentin Zberea <valentin.zberea@gmail.com>
2018-12-12 22:47:25 +0100

    Day 2 problem

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -1,18 +1,61 @@
 defmodule Day2 do
-  @moduledoc """
-  Documentation for Day2.
-  """
+  def checksum_from_file() do
+    {:ok, content} = File.read("./priv/input.txt")
+    checksum(content)
+  end
 
-  @doc """
-  Hello world.
+  def checksum(input, separator \\ "\n") do
+    multiple_occurences =
+      input
+      |> String.split(separator, trim: true)
+      |> Enum.map(&String.trim(&1))
+      |> Enum.map(&contains_multiple_letters(&1))
 
-  ## Examples
+    count_of_2 = multiple_occurences |> Enum.count(&(2 in &1))
+    count_of_3 = multiple_occurences |> Enum.count(&(3 in &1))
 
-      iex> Day2.hello()
-      :world
+    count_of_2 * count_of_3
+  end
 
-  """
-  def hello do
-    :world
+  def contains_multiple_letters(id) do
+    id
+    |> String.codepoints()
+    |> Enum.group_by(& &1)
+    |> Map.values()
+    |> Enum.map(&Enum.count(&1))
+    |> Enum.group_by(& &1)
+    |> Map.keys()
+    |> Enum.filter(&(&1 in [2, 3]))
+  end
+
+  def common_letters_from_file() do
+    {:ok, content} = File.read("./priv/input.txt")
+    common_letters(content)
+  end
+
+  def common_letters(input, separator \\ "\n") do
+    ids =
+      input
+      |> String.split(separator, trim: true)
+      |> Enum.map(&String.trim(&1))
+
+    ids
+    |> Enum.flat_map(fn x -> ids |> Enum.map(fn y -> [x, y] end) end)
+    |> Enum.filter(fn [x, y] -> String.myers_difference(x, y) |> Enum.count() == 4 end)
+    |> Enum.filter(fn [x, y] ->
+      String.myers_difference(x, y)
+      |> Enum.find(fn {t, _} -> t == :ins end)
+      |> Tuple.to_list()
+      |> Enum.at(1)
+      |> String.length() == 1
+    end)
+    |> Enum.at(0)
+    |> common_letters_result()
+  end
+
+  defp common_letters_result([a, b]) do
+    String.myers_difference(a, b)
+    |> Enum.filter(fn {t, _} -> t == :eq end)
+    |> Enum.map_join(fn {_, str} -> str end)
   end
 end
--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -2,7 +2,43 @@ defmodule Day2Test do
   use ExUnit.Case
   doctest Day2
 
-  test "greets the world" do
-    assert Day2.hello() == :world
+  test "id has a letter appearing 3 times" do
+    id = "aava"
+    assert Day2.contains_multiple_letters(id) == [3]
+  end
+
+  test "id has a letter appearing 2 times" do
+    id = "ava"
+    assert Day2.contains_multiple_letters(id) == [2]
+  end
+
+  test "id has no multiple letters" do
+    id = "avb"
+    assert Day2.contains_multiple_letters(id) == []
+  end
+
+  test "checksum" do
+    ids = """
+    aa
+    bbb
+    aabbccc
+    """
+
+    assert Day2.checksum(ids) == 4
+  end
+
+  test "common letters" do
+    input = """
+    axyde
+    abcde
+    fghij
+    klmno
+    pqrst
+    fguij
+    axcye
+    wvxyz
+    """
+
+    assert "fgij" == Day2.common_letters(input)
   end
 end
